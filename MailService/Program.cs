bf57e447b5d33904b254f4a7ceb530c4999f6694
hyegen <hyegen205@gmail.com>
2023-12-17 23:34:56 +0300

    Add project files.

---
+++ MailService/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace MailService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string senderEmail = "sender@gmail.com";
+            string senderPassword = "password";
+          
+            string toEmail = "receiver@gmail.com";
+
+            string subject = "Test e-mail";
+            string body = "Hello, This is a test e-mail";
+          
+            string smtpServer = "smtp.gmail.com";
+            int smtpPort = 587;    //It can be changes. (25,465) 
+
+            try
+            {
+                using (MailMessage mail = new MailMessage())
+                {
+                    mail.From = new MailAddress(senderEmail);
+                    mail.To.Add(toEmail);
+                    mail.Subject = subject;
+                    mail.Body = body;
+                    mail.IsBodyHtml = false;
+
+                    using (SmtpClient smtp = new SmtpClient(smtpServer, smtpPort))
+                    {
+                        smtp.Credentials = new NetworkCredential(senderEmail, senderPassword);
+                        smtp.EnableSsl = true;
+
+                        smtp.Send(mail);
+                    }
+
+                    Console.WriteLine("E-mail has been sent successfuly");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while sending the email: " + ex.Message);
+            }
+        }
+    }
+}
